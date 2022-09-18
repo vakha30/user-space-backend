82577cd1be618d98505c0ae9e1c4c3470305c3a9
Ваха Ирзаханов <v.irzahanov@ideco.ru>
2022-09-18 18:14:16 +0300

    usb-3 Get user information

--- auth_backend/views.py
+++ auth_backend/views.py
@@ -1,10 +1,20 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, UserSerializer
 
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
