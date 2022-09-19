af6cba55b1418ae98aaf150ae0c62a211f9fb10d
Ваха Ирзаханов <v.irzahanov@ideco.ru>
2022-09-20 00:18:42 +0300

    Add id

--- auth_backend/serializers.py
+++ auth_backend/serializers.py
@@ -46,4 +46,4 @@ class RegisterSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("username", "email", "first_name", "last_name")
+        fields = ("id", "username", "email", "first_name", "last_name")
