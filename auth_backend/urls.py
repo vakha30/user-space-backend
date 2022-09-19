f1b132a3c151aca2c0e3a378e11d444cbfbc85c8
Ваха Ирзаханов <v.irzahanov@ideco.ru>
2022-09-19 23:46:28 +0300

    usb-5 Переделать аутентификацию на JWT

--- auth_backend/urls.py
+++ auth_backend/urls.py
@@ -1,9 +1,16 @@
 from django.urls import include, path
 from auth_backend.views import RegisterView, UserDetail, UserList
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
-    path("", include("rest_framework.urls")),
     path("register/", RegisterView.as_view(), name="auth_register"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("users/", UserList.as_view()),
     path("users/<int:pk>", UserDetail.as_view()),
 ]
